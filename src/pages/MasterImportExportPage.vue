f573411b74b269c3c8578b8cfbabfc6aef1fccfa
Shigeru Hagiwara <hgwrsgr@gmail.com>
2025-04-05 18:53:27 +0900

    stub

---
+++ src/pages/MasterImportExportPage.vue
@@ -0,0 +1,9 @@
+<template>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+/* Add your styles here */
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,62 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
+
+import HomePage from '@/pages/HomePage.vue'
+import MasterListPage from '@/pages/MasterListPage.vue'
+import MasterEditPage from '@/pages/MasterEditPage.vue'
+import MasterImportExportPage from '@/pages/MasterImportExportPage.vue'
+import ActiveListPage from '@/pages/ActiveListPage.vue'
+import FinishedListPage from '@/pages/FinishedListPage.vue'
+import ChecklistDetailPage from '@/pages/ChecklistDetailPage.vue'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomePage,
+  },
+  {
+    path: '/masters',
+    name: 'master-list',
+    component: MasterListPage,
+  },
+  {
+    path: '/masters/new',
+    name: 'master-create',
+    component: MasterEditPage,
+  },
+  {
+    path: '/masters/:id',
+    name: 'master-edit',
+    component: MasterEditPage,
+    props: true,
+  },
+  {
+    path: '/masters/import-export',
+    name: 'master-import-export',
+    component: MasterImportExportPage,
+  },
+  {
+    path: '/actives',
+    name: 'active-list',
+    component: ActiveListPage,
+  },
+  {
+    path: '/actives/:id',
+    name: 'active-detail',
+    component: ChecklistDetailPage,
+    props: true,
+  },
+  {
+    path: '/finished',
+    name: 'finished-list',
+    component: FinishedListPage,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
